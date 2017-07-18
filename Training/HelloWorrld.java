48130ba64a4734931f1cef99757983e664dce76a
OleksandrTurbovets <oleksandr.turbovets@gmail.com>
2017-07-18 19:10:20 +0300

    Refactoring and LightSpeed

--- Training/HelloWorrld.java
+++ Training/HelloWorrld.java
@@ -1,4 +1,6 @@
-public class Main {
+package Training;
+
+public class HelloWorrld {
     public static void main(String[] args) {
         System.out.println("Hello world2");
     }
---
+++ Training/LightSpeed.java
@@ -0,0 +1,14 @@
+package Training;
+
+public class LightSpeed {
+    public static void main(String[] args) {
+        double lightSpeed = 299792458;
+        double nanoSecond = 1000000000;
+        double result = lightSpeed * 100 / nanoSecond;
+        System.out.println(result);
+
+        double ops = 2200000000d;
+        double resultOps = lightSpeed * 100 / ops;
+        System.out.println(resultOps);
+    }
+}
