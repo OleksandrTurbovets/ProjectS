3671c5db328d2851828c4f946689ce8561b24a62
OleksandrTurbovets <oleksandr.turbovets@gmail.com>
2017-08-09 18:02:02 +0300

    Upcasting and Downcasting

---
+++ Training/UpcastingDownCasting.java
@@ -0,0 +1,15 @@
+public class UpcastingDownCasting {
+    public static void main(String[] args) {
+//        int intValue = 10;
+//        double doubleValue = 10.5;
+//        doubleValue = intValue;
+//        intValue = (int) doubleValue;
+
+        int sum = 2;
+        double cv = 3.09;
+        sum += 10*cv;
+
+
+        System.out.println(sum);
+    }
+}
